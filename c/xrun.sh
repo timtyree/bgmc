0aa9c3abf70bc2605e1ddc3793f32de307ac6741
timtyree <tjgtyree@aol.com>
2021-03-26 19:19:42 -0700

    update random seed generation

--- c/xrun.sh
+++ c/xrun.sh
@@ -1,7 +1,7 @@
 date
-echo "$1 $2 $3 $4 $5 " > params.input
+echo "$1 $2 $3 $4 $5 $6" > params.input
 # export UNIQUE_SEED=$(process)
-echo $(process) >> params.input
+# echo $(Process) >> params.input
 # gcc return_CollTimes.c -o return_CollTimes.x -lm
-# ./return_CollTimes.x < params.input
+./return_CollTimes.x < params.input
 date
