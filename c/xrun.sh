d2f412b931f9a01c8ae2a720ada786f2b53fbf38
timtyree <tjgtyree@aol.com>
2021-03-26 19:07:56 -0700

    updated local params.input

--- c/xrun.sh
+++ c/xrun.sh
@@ -1,8 +1,7 @@
 date
 echo "$1 $2 $3 $4 $5 " > params.input
 # export UNIQUE_SEED=$(process)
-UNIQUE_SEED=$(process)
-echo "$(UNIQUE_SEED)" >> params.input 
+echo $(process) >> params.input
 # gcc return_CollTimes.c -o return_CollTimes.x -lm
-./return_CollTimes.x < params.input
+# ./return_CollTimes.x < params.input
 date
