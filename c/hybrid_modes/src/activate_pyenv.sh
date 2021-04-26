fdc0526bd7c13044277cf67d5a11ef424c3c1492
timtyree <tjgtyree@aol.com>
2021-04-26 09:16:22 -0700

    update hybrid_modes submission

--- c/hybrid_modes/src/activate_pyenv.sh
+++ c/hybrid_modes/src/activate_pyenv.sh
@@ -4,4 +4,4 @@ module load python/3.7.0
 #unpack virtual python environment and activate it
 tar -xzf pyenv.tar.gz
 python3 -m venv pyenv
-source pyenv/bin/activate &> ignore.txt
+source pyenv/bin/activate # &> ignore.txt
