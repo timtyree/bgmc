2375e386e0367ea6948c92eaeef0d06925fb00ec
timtyree <tjgtyree@aol.com>
2021-05-15 16:28:40 -0700

    Update summarize.sh

--- c/ou/summarize.sh
+++ c/ou/summarize.sh
@@ -5,18 +5,18 @@ count=0
 niter=$6
 Process=$7
 # $(r) $(D) $(L) $(kappa) $(Dt) $(niter) $(Process) $(reflect) $(set_second)
-$(1) $(2) $(3) $(4) $(5) $(niter_batchsize) $(Process_batch) $(8) $(9)
+# $(1) $(2) $(3) $(4) $(5) $(niter_batchsize) $(Process_batch) $(8) $(9)
 
 niter_batchsize=10
 Process_batch=Process
-numberof batches
+numberof_batches=$(niter/niter_batchsize)
 Nmin=11
 Nmax=200
-for file in "$@"
+# run the simulation with a small cache size
 while (niter_net<niter)
   ./xrun.sh $(1) $(2) $(3) $(4) $(5) $(niter_batchsize) $(Process_batch) $(8) $(9) > out.txt
-  # TODO: search out.txt for -9999 in the first 5 characters of the line
-  # TODO: explicitly find the line that starts with -9999.  declare ^this line to be the output_line
+  # explicitly find the output
+  grep -n -E 'Printing Outputs...' out.txt > lineno.txt
   # TODO: add ^this output line to the sum, which is listed in the only line of sum.out
   # TODO:
   # TODO: redirect ^that output in a meaningful way
