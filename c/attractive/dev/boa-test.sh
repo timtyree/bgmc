26bf66064e4816e2c8ffc6993d5b8b1a6ffdd95f
Tim Tyree <tjt57@cornell.edu>
2021-08-09 13:04:58 -0700

    generated repeatable tests for finite basin of attraction

--- c/attractive/dev/boa-test.sh
+++ c/attractive/dev/boa-test.sh
@@ -1,4 +1,4 @@
-N=20
+N=1000
 FN='tmp.input'
 clear
 cd ..
@@ -9,7 +9,7 @@ echo "/* Testing Effect of Adding a 1cm Basin of Attraction between (i) nearest 
 
 echo """Key Results made repeatable here:
   1. when using reflective boundary conditions, a small constant repulsive force decreased the annihilation rate independent of whether or not forces were only considered if they were between nearest neighbors.
-  2. if periodic boundary conditions were used, however, a small constant repulsive force _increased_ the annihilation rate if the forces were considered as a vector sum between all particles.
+  2. if periodic boundary conditions were used and the forces were considered as a vector sum between all particles, a small constant repulsive force _increased_ the annihilation rate for 1/r forces but not 1/r^2 forces.
 
 This same result emerged independent of 1/r forces or 1/r^2 forces being used.
 """
@@ -24,7 +24,7 @@ echo 0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 0 0 0 0 1 2 > $FN
 ./../return_CollTime.x < $FN | grep "Tavg"
 echo 0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 0 0 0 0 1 4 > $FN
 ./../return_CollTime.x < $FN | grep "Tavg"
-echo "If Tavg got lower, then the rate of collision was _increased_ by a constant repulsive force.\n"
+echo "If Tavg got higher, then the rate of collision was decreased by a constant repulsive force.\n"
 
 echo "/***Case 2: vector summed forces***/"
 echo "Printing the mean time to first reaction (seconds) before/after adding a constant repulsive force for a 1cm basin of attraction..."
@@ -33,7 +33,7 @@ echo 0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 0 0 0 0 0 2 > $FN
 ./../return_CollTime.x < $FN | grep "Tavg"
 echo 0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 0 0 0 0 0 4 > $FN
 ./../return_CollTime.x < $FN | grep "Tavg"
-echo "If Tavg got higher, then the rate of collision was decreased by a constant repulsive force.\n"
+echo "If Tavg got lower, then the rate of collision was _increased_ by a constant repulsive force.\n"
 
 echo "\n/* * * turning on inverse squared power law forces * * */\n"
 
@@ -43,7 +43,7 @@ echo 0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 1 0 0 0 1 3 > $FN
 ./../return_CollTime.x < $FN | grep "Tavg"
 echo 0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 1 0 0 0 1 5 > $FN
 ./../return_CollTime.x < $FN | grep "Tavg"
-echo "If Tavg got lower, then the rate of collision was _increased_ by a constant repulsive force.\n"
+echo "If Tavg got higher, then the rate of collision was decreased by a constant repulsive force.\n"
 
 echo "/***Case 4: vector summed forces with inverse square powerlaw attraction***/"
 echo "Printing the mean time to first reaction (seconds) before/after adding a constant repulsive force for a 1cm basin of attraction..."
@@ -54,7 +54,7 @@ echo 0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 1 0 0 0 0 5 > $FN
 ./../return_CollTime.x < $FN | grep "Tavg"
 echo "If Tavg got higher, then the rate of collision was decreased by a constant repulsive force.\n"
 
-echo "\n/* * * turning on reflecing boundary conditions and inverse power law forces * * */\n"
+echo "\n/* * * turning on reflecting boundary conditions and inverse power law forces * * */\n"
 
 echo "/***Case 5: nearest neighbor summed forces***/"
 echo "Printing the mean time to first reaction (seconds) before/after adding a constant repulsive force for a 1cm basin of attraction..."
@@ -77,18 +77,18 @@ echo "\n/* * * turning on inverse squared power law forces * * */\n"
 
 echo "/***Case 7: nearest neighbor summed forces with inverse square powerlaw attraction***/"
 echo "Printing the mean time to first reaction (seconds) before/after adding a constant repulsive force for a 1cm basin of attraction..."
-echo '0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 1 0 0 0 1 3' > $FN
+echo 0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 1 0 0 0 1 3 > $FN
 ./../return_CollTime.x < $FN | grep "Tavg"
-echo '0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 1 0 0 0 1 5' > $FN
+echo 0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 1 0 0 0 1 5 > $FN
 ./../return_CollTime.x < $FN | grep "Tavg"
 echo "If Tavg got higher, then the rate of collision was decreased by a constant repulsive force.\n"
 
 echo "/***Case 8: vector summed forces with inverse square powerlaw attraction***/"
 echo "Printing the mean time to first reaction (seconds) before/after adding a constant repulsive force for a 1cm basin of attraction..."
 FN='tmp.input'
-echo '0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 1 0 0 0 0 3' > $FN
+echo 0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 1 0 0 0 0 3 > $FN
 ./../return_CollTime.x < $FN | grep "Tavg"
-echo '0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 1 0 0 0 0 5' > $FN
+echo 0.1 2 5 15000 20 1 1e-05 1e-05 10 ${N} 1234 1 0 0 0 0 5 > $FN
 ./../return_CollTime.x < $FN | grep "Tavg"
 echo "If Tavg got higher, then the rate of collision was decreased by a constant repulsive force.\n"
 date
