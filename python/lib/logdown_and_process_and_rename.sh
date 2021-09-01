997a281be410bab66d1e6ac68b5be84cece96746
timtyree <tjgtyree@aol.com>
2021-08-31 22:30:48 -0700

    dev K-surviving genetic algorithm

--- python/lib/logdown_and_process_and_rename.sh
+++ python/lib/logdown_and_process_and_rename.sh
@@ -2,7 +2,7 @@
 ./logdown_and_process.sh
 cd ../data/osg_output
 #TOOD: add support for string parsing, 14-->$RUN_NUMBER
-mv run_X_all run_14_all
-mv run_X_all.csv run_14_all.csv
+mv run_X_all run_15_all
+mv run_X_all.csv run_15_all.csv
 #TODO: integrate m,M observation into process
 #output row maps all trial parameters to the apparent m,M with their summary statistics
--- python/lib/viewer/mark_slope.py
+++ python/lib/viewer/mark_slope.py
@@ -2,28 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from mpltools import annotation
+def mark_slope(ax, xy1=(10,2),xy2=(1,2), text_kwargs={'color': 'cornflowerblue'},poly_kwargs={'facecolor': (0.73, 0.8, 1)},**kwargs):
+    annotation.slope_marker((10, 2), (1, 2), ax=ax2,
+                            text_kwargs=text_kwargs,
+                            poly_kwargs=poly_kwargs,**kwargs)
+    return ax
 
-
-x = np.logspace(0, 2)
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-
-ax1.plot([0, 2], [1, 0])
-annotation.slope_marker((1, 0.6), (-1, 2), ax=ax1)
-ax1.set_title('linear, negative slope')
-
-ax2.loglog(x, x**0.5)
-annotation.slope_marker((10, 2), (1, 2), ax=ax2,
-                        text_kwargs={'color': 'cornflowerblue'},
-                        poly_kwargs={'facecolor': (0.73, 0.8, 1)})
-ax2.set_title('loglog, custom colors')
-
-ax3.loglog(x, x**0.5)
-annotation.slope_marker((10, 4), (1, 2), invert=True, ax=ax3)
-ax3.set_title('loglog, `invert=True`')
-
-ax4.loglog(x, x**0.5)
-annotation.slope_marker((10, 2), 0.5, ax=ax4)
-ax4.set_title('loglog, float slope')
-
-plt.tight_layout()
-plt.show()
+# # forked from stackoverflow
+# x = np.logspace(0, 2)
+# fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+#
+# ax1.plot([0, 2], [1, 0])
+# annotation.slope_marker((1, 0.6), (-1, 2), ax=ax1)
+# ax1.set_title('linear, negative slope')
+#
+# ax2.loglog(x, x**0.5)
+# annotation.slope_marker((10, 2), (1, 2), ax=ax2,
+#                         text_kwargs={'color': 'cornflowerblue'},
+#                         poly_kwargs={'facecolor': (0.73, 0.8, 1)})
+# ax2.set_title('loglog, custom colors')
+#
+# ax3.loglog(x, x**0.5)
+# annotation.slope_marker((10, 4), (1, 2), invert=True, ax=ax3)
+# ax3.set_title('loglog, `invert=True`')
+#
+# ax4.loglog(x, x**0.5)
+# annotation.slope_marker((10, 2), 0.5, ax=ax4)
+# ax4.set_title('loglog, float slope')
+#
+# plt.tight_layout()
+# plt.show()
