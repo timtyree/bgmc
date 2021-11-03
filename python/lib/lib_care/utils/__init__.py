87f6dc1866390670f828ead87dd62b59d1a4c7ca
timtyree <tjgtyree@aol.com>
2021-11-03 11:16:06 -0700

    slimmed lib to relieve remote cython compilation dependency

--- python/lib/lib_care/utils/__init__.py
+++ python/lib/lib_care/utils/__init__.py
@@ -4,20 +4,20 @@ from .dist_func import *
 from .ProgressBar import *
 from .utils_jsonio import *
 from .utils_traj import *
-from .utils_tips import *
-from .utils_contours import *
-from .chunk_traj import *
-from .stack_txt_LR import *
+# from .utils_tips import *
+# from .utils_contours import *
+# from .chunk_traj import *
+# from .stack_txt_LR import *
 from .operari import *
-from .ParticleClasses import *
+# from .ParticleClasses import *
 
 #for worker
-from .get_txt import *
-from .chunk_array import *
-from .zoom_array import *
-# from .make_directories import *
-from .make_worker_directories import *
-
-#these might conflict when generating LR ic, otherwise should be good with _LR first...
-from .load_buffer_LR import *
-from .operari import load_buffer
+# from .get_txt import *
+# from .chunk_array import *
+# from .zoom_array import *
+# # from .make_directories import *
+# from .make_worker_directories import *
+#
+# #these might conflict when generating LR ic, otherwise should be good with _LR first...
+# from .load_buffer_LR import *
+# from .operari import load_buffer
