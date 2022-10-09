b416b2a43e9995c4d0d58fa1ce6e837aae9cf72e
timtyree <tjgtyree@aol.com>
2022-10-09 12:59:07 -0700

    dev run 31 at 4X larger domain size

---
+++ c/attractive/src/gen_run_31.sh
@@ -0,0 +1,3 @@
+python3 gen_run_31.py > ../runs/run_31.dat
+cd ../runs
+tail -4 run_31.dat > run_test.dat
--- python/lib/logdown_and_process_and_rename.sh
+++ python/lib/logdown_and_process_and_rename.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-run_number=26
+run_number=30
 
 read -p "Is the current run_number=${run_number}? (y/n)" answer
 
