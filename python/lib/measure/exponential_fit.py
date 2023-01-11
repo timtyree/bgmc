b65f931370528571263132a47a3d1a585bb8238b
timtyree <tjgtyree@aol.com>
2023-01-10 16:45:56 -0800

    determined characteristic areas for exponential termination scaling law

--- python/lib/measure/exponential_fit.py
+++ python/lib/measure/exponential_fit.py
@@ -1,6 +1,13 @@
 import numpy as np
 from .compute_slope import *
 def fit_exponential(x,y):
+    """fits an exponential trace by OLS.
+
+    Example Usage:
+B,Delta_B,m,Delta_m,Rsq=fit_exponential(x,y)
+rmse=compute_exponential_rmse(x,y,m,B)
+#now see print_fit_exponential(x,y)
+    """
     dict_linlog=compute_95CI_ols(x,np.log(y))
     m=dict_linlog['m']
     Delta_m=dict_linlog['Delta_m']
@@ -10,7 +17,14 @@ def fit_exponential(x,y):
     return B,Delta_B,m,Delta_m,Rsq
 
 def compute_exponential_rmse(x_values,y_values,m,B):
-    """compute rmse of power law fits"""
+    """compute rmse of power law fits
+
+    Example Usage:
+B,Delta_B,m,Delta_m,Rsq=fit_exponential(x,y)
+yvhat=-B*np.exp(xv*m)
+rmse=np.sqrt(np.mean((yvhat-yv)**2))
+compute_exponential_rmse(x_values,y_values,m,-B)
+    """
     yv=B*np.exp(x_values*m)
     rmse=np.sqrt(np.mean((y_values-yv)**2))
     return rmse
